b19048736c82c6c0bc002307b1c4b921135e6cc9
Ali Momin <Ali@Alis-MBP-2.attlocal.net>
2023-02-11 16:40:43 -0500

    fixed weather icon not showing

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 var APIkey = 'c40b28aa33c2bef2881ab9e4f13c3ef7';
 //var requestURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=' + APIkey
 
-var cityName = document.getElementById("cityName")
-var searchBtn = document.getElementById("searchBtn")
+var cityName = document.getElementById("cityName");
+var searchBtn = document.getElementById("searchBtn");
 var currentWeatherContainer = document.getElementById("currentWeather");
-var fiveDayWeatherContainer = document.getElementById("fiveDayWeather")
+var fiveDayWeatherContainer = document.getElementById("fiveDayWeather");
+var searchHistory = document.getElementById("searchHistory");
 
 function getLatLong (city){
     var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey
@@ -30,7 +31,7 @@ function getForecast (lat, lon){
 
         // 1. Create the element
         var currentCity = document.createElement('h2');
-        //var currentDate = document.createElement('h2');
+        var currentDate = document.createElement('h2');
         var currentIcon = document.createElement('img');
         var currentTemp = document.createElement('p');
         var currentWind = document.createElement('p');
@@ -39,7 +40,7 @@ function getForecast (lat, lon){
         // 2. Give it content
         currentCity.textContent = data.city.name;
         // currentDate.textContent = (data.list[0].dt).format("MM/DD/YYYY");
-        currentIcon.setAttribute("scr",'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
+        currentIcon.setAttribute("src",'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png')
         currentTemp.innerHTML = "Temp: <span>" + data.list[0].main.temp + " F</span>";
         currentWind.innerHTML = "Wind: <span>" + data.list[0].wind.speed + " MPH</span>";
         currentHumidity.innerHTML = "Humidity: <span>" + data.list[0].main.humidity + " %</span>";
@@ -49,21 +50,23 @@ function getForecast (lat, lon){
         currentCity.setAttribute('class', 'title')
 
         // 4. Append the new element to the page
-        currentWeatherContainer.append(currentCity, currentIcon, currentTemp, currentWind, currentHumidity)
+        currentWeatherContainer.append(currentCity, currentDate, currentIcon, currentTemp, currentWind, currentHumidity)
         
 
         for (var i =1; i < 6; i++){
             var futureIcon = document.createElement('img');
+            var futureDate = document.createElement('h2');
             var futureTemp = document.createElement('p');
             var futureWind = document.createElement('p');
             var futureHumidity = document.createElement('p');
 
-            futureIcon.setAttribute("scr",'https://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png')
+            futureIcon.setAttribute("src",'https://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png')
+            //futureDate.textContent = 
             futureTemp.innerHTML = "Temp: <span>" + data.list[i].main.temp + " F</span>";
             futureWind.innerHTML = "Wind: <span>" + data.list[i].wind.speed + " MPH</span>";
             futureHumidity.innerHTML = "Humidity: <span>" + data.list[i].main.humidity + " %</span>";
 
-            fiveDayWeatherContainer.append(futureIcon, futureTemp, futureWind, futureHumidity)
+            fiveDayWeatherContainer.append(futureIcon, futureDate, futureTemp, futureWind, futureHumidity)
         }
 
 
